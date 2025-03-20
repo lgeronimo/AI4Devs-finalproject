a01fc4f4ed88b1b25b16fbe2b61e8feb237838d0
lgeronimo <luis.geronimov@gmail.com>
2025-03-20 04:03:04 -0600

    feature(voice-command): new style

--- frontend/easy-reader/src/app/features/reader/components/voice-command/voice-command.component.ts
+++ frontend/easy-reader/src/app/features/reader/components/voice-command/voice-command.component.ts
@@ -1,7 +1,14 @@
-import { Component, OnInit, OnDestroy, inject } from '@angular/core';
+import { Component, OnInit, OnDestroy, inject, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PdfService } from '../../services/pdf.service';
 import { Subject } from 'rxjs';
+
+interface MicState {
+  message: string;
+  iconClass: string;
+  iconColor: string;
+  iconBackgroundColor?: string;
+}
 
 @Component({
   selector: 'app-voice-command',
@@ -14,35 +21,91 @@ export class VoiceCommandComponent implements OnInit, OnDestroy {
   private recognition: any;
   isListening: boolean = false;
   private commands: Set<string> = new Set(['next', 'down', 'up', 'top', 'bottom']);
-  private destroy$ = new Subject<void>()
+  private destroy$ = new Subject<void>();
   private pdfService = inject(PdfService);
+  private cdr = inject(ChangeDetectorRef);
+  private hasError: boolean = false;
+
+  micState: MicState = {
+    message: 'Please authorize microphone access to proceed',
+    iconClass: 'fa-microphone', 
+    iconColor: 'white',
+    iconBackgroundColor: '#6366F1'
+  };
+
+  micStates: Record<string, MicState> = {
+    default: this.micState,
+    onstart: {
+      message: 'Listening...',
+      iconClass: 'fa-microphone',
+      iconColor: 'white',
+      iconBackgroundColor: '#10B981'
+    },
+    onend: {
+      message: 'Click to speak again',
+      iconClass: 'fa-microphone-slash',
+      iconColor: 'white',
+      iconBackgroundColor: '#6B7280'
+    },
+    onerror: {
+      message: 'Microphone access is required. Please enable it in your browser settings to continue.',
+      iconClass: 'fa-microphone-slash',
+      iconColor: 'white',
+      iconBackgroundColor: '#EF4444'
+    }
+  };
 
   ngOnInit(): void {
-    if ('webkitSpeechRecognition' in window) {
-      this.recognition = new (window as any).webkitSpeechRecognition();
+    if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
+      const SpeechRecognition = (window as any).SpeechRecognition || (window as any).webkitSpeechRecognition;
+      this.recognition = new SpeechRecognition();
       this.recognition.continuous = true;
       this.recognition.lang = 'en-US';
+      this.recognition.interimResults = false; 
 
       this.recognition.onstart = () => {
-        console.log('onstart');
         this.isListening = true;
+        this.hasError = false;
+        console.log('event: onstart'); 
+        this.updateMicState('onstart');
       };
 
       this.recognition.onend = () => {
-        console.log('onend');
-        this.isListening = false;
+        if (!this.hasError) {
+          this.isListening = false;
+          console.log('event: onend');
+          this.updateMicState('onend');
+        }
       };
 
       this.recognition.onresult = (event: any) => {
-        const command = event.results[event.resultIndex][0].transcript.trim().toLowerCase();
-        if (this.commands.has(command)) {
-          this.handleCommand(command);
-        } else {
-          console.log(`Comando no reconocido: ${command}`);
+        let finalTranscript = '';
+        console.log('onresult');
+        for (let i = event.resultIndex; i < event.results.length; i++) {
+          const transcript = event.results[i][0].transcript.trim().toLowerCase();
+          if (transcript && event.results[i].isFinal) {
+            finalTranscript += transcript;
+            if (this.commands.has(transcript)) {
+              this.handleCommand(transcript);
+            } else {
+              console.log(`Comando no reconocido: ${transcript}`);
+            }
+          }
+        }
+        // this.updateMicState('stopped');
+      };
+
+      this.recognition.onerror = (event: any) => {
+        console.log('event: onerror:', event);
+        if (event.error === 'not-allowed') {
+          this.hasError = true;
+          this.recognition.abort();
+          this.updateMicState('onerror');
         }
       };
     } else {
-      console.error('Speech recognition not supported in this browser.');
+      console.log('event: no-speech');
+      this.updateMicState('onerror');
     }
   }
 
@@ -52,9 +115,10 @@ export class VoiceCommandComponent implements OnInit, OnDestroy {
     }
   }
 
-  stopListening(): void {
+  abortListening(): void {
     if (this.recognition && this.isListening) {
-      this.recognition.stop();
+      this.recognition.abort();
+      this.updateMicState('stopped');
     }
   }
 
@@ -80,8 +144,14 @@ export class VoiceCommandComponent implements OnInit, OnDestroy {
     }
   }
 
+  updateMicState(state: string): void {
+    console.log('event: updateMicState', state);
+    this.micState = this.micStates[state] || this.micStates["default"];
+    this.cdr.detectChanges();
+  }
+
   ngOnDestroy(): void {
-    this.stopListening();
+    this.abortListening();
     this.destroy$.next();
     this.destroy$.complete();
   }
