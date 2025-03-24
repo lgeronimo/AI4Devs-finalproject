46896444981baeb8fbe320df5875996fcf668e83
lgeronimo <luis.geronimov@gmail.com>
2025-03-23 20:24:38 -0600

    fix: route fixed

--- finalproject-LAGV/frontend/easy-reader/src/app/app.routes.ts
+++ finalproject-LAGV/frontend/easy-reader/src/app/app.routes.ts
@@ -28,6 +28,6 @@ export const routes: Routes = [
     path: 'auth',
     loadChildren: () => import('./features/auth/auth.routes').then(m => m.AUTH_ROUTES)
   },
-  { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
-  { path: '**', redirectTo: '/auth/login' }
+  { path: '', redirectTo: '/auth', pathMatch: 'full' },
+  { path: '**', redirectTo: '/auth' }
 ];
